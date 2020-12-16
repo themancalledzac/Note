882cf248c68823e21898f8f0a3f541ae03ec5ced
Zac Edens <themancalledzac@gmail.com>
2020-12-15 23:58:11 -0800

    server now functional.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// this should be able to load all files in that directory? or will i need to do it for each folder within?
+app.use(express.static('public'));
+app.use(express.static('routes'));
+
+
+// require("./routes/apiRoutes")(app);
+
+
+app.listen( PORT );
+
+// http://localhost:3002/
